6a6a2f335504b79f9bbf5319af0c0a3d7e835bef
keven-lee <kevenlee123@gmail.com>
2016-04-23 22:44:43 -0400

    Refactored

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-  for (var i = 1; i <= 100; i++) {
+  var userInput = prompt("Please enter a number");
+  var max = parseInt(userInput);
+
+function fizzbuzz(max) {
+  for (var i = 1; i <= max; i++) {
   	if (i % 3 == 0 && i % 5 == 0) {
   	  $('.list').append('<li>' + 'fizzbuzz' + '</li>');
   	}
@@ -13,4 +17,8 @@ $(document).ready(function(){
   	  $('.list').append('<li>' + i + '</li>');
   	}
   }
+}
+
+fizzbuzz(max)
+
 });
